c510526ede9d7a4fa4dd334985209e47ef433a54
cbshields <cbshields@outlook.com>
2020-02-05 11:23:11 -0700

    Initiated dealership controller methods

--- app/controllers/api/v1/dealerships_controller.rb
+++ app/controllers/api/v1/dealerships_controller.rb
@@ -1,2 +1,41 @@
 class Api::V1::DealershipsController < ApplicationController
+
+
+    def index
+      @dealerships = Dealership.all
+      render json: @dealerships
+    end
+
+    def create
+      @dealership = Dealership.new(dealership_params)
+      if @dealership.save
+        render json: @dealership
+      else
+        render json: {error: 'Error entering dealership information'}
+    end
+
+    def show
+      @dealership = Dealership.find(params[:id])
+      render json: @dealership
+    end
+
+    def destroy
+      @dealership = Dealership.find(params[:id])
+      @dealership.delete
+      # Should all cars get deleted?
+
+    end
+
+    private
+    def dealership_params
+      params.require(:dealership).permit(
+        :name,
+        :address,
+        :city,
+        :state,
+        :zip,
+        :phone,
+        :inventory
+      )
+    end
 end
