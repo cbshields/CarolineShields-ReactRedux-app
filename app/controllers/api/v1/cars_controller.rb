ee6e0ebbac8b486165dc42a015d2cd79fe134755
cbshields <cbshields@outlook.com>
2020-02-05 11:01:31 -0700

    Initiated cars controller methods

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,2 +1,44 @@
 class Api::V1::CarsController < ApplicationController
+
+  def index
+    @cars = Car.all
+    render json: @cars
+  end
+
+  def create
+    @car = Car.new(car_params)
+    if @car.save
+      render json: @account
+    else
+      render json: {error: 'Error entering car information'}
+  end
+
+  def show
+    @car = Car.find(params[:id])
+    render json: @car
+  end
+
+  def destroy
+    @car = Car.find(params[:id])
+    @dealer_id = @car.dealership_id
+    # @car.delete
+    # update car inventory
+    # @dealership = Dealership.find(@dealer_id)
+    # @dealership = @dealership.inventory - 1
+    # @dealership.save
+  end
+
+  private
+  def car_params
+    params.require(:car).permit(
+      :make,
+      :model,
+      :year,
+      :color,
+      :price,
+      :available,
+      :dealership_id
+    )
+  end
+
 end
